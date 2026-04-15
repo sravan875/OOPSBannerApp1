e1cb5e45f4e94a642e65a18b48a0d152eec1176d
sravan875 <sravankumarkona875@gmail.com>
2026-04-15 15:27:45 +0530

    feature 4:banner using string array and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,32 +1,24 @@
 /**
- * OOPSBannerApp UC3 - OOPS Banner Application (Use Case 3)
- * Uses String.join() instead of + operator for better readability and efficiency
+ * UC4 - OOPS Banner using Array and Loop
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Each line represents O O P S
+        // Store all 7 lines in an array
+        String[] banner = new String[7];
 
-        System.out.println(String.join(" ",
-                "  ***   ", "  ***   ", " *****  ", "  ****  "));
+        banner[0] = String.join(" ", "  ***   ", "  ***   ", " *****  ", "  ****  ");
+        banner[1] = String.join(" ", " ** **  ", " ** **  ", " **  ** ", " **     ");
+        banner[2] = String.join(" ", "**   ** ", "**   ** ", " **  ** ", " **     ");
+        banner[3] = String.join(" ", "**   ** ", "**   ** ", " *****  ", "  ****  ");
+        banner[4] = String.join(" ", "**   ** ", "**   ** ", " **     ", "     ** ");
+        banner[5] = String.join(" ", " ** **  ", " ** **  ", " **     ", "     ** ");
+        banner[6] = String.join(" ", "  ***   ", "  ***   ", " **     ", " ****   ");
 
-        System.out.println(String.join(" ",
-                " ** **  ", " ** **  ", " **  ** ", " **     "));
-
-        System.out.println(String.join(" ",
-                "**   ** ", "**   ** ", " **  ** ", " **     "));
-
-        System.out.println(String.join(" ",
-                "**   ** ", "**   ** ", " *****  ", "  ****  "));
-
-        System.out.println(String.join(" ",
-                "**   ** ", "**   ** ", " **     ", "     ** "));
-
-        System.out.println(String.join(" ",
-                " ** **  ", " ** **  ", " **     ", "     ** "));
-
-        System.out.println(String.join(" ",
-                "  ***   ", "  ***   ", " **     ", " ****   "));
+        // Print using loop
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
