2a044a8e7b0432c6b799f942ca298b27971883e9
sravan875 <sravankumarkona875@gmail.com>
2026-04-15 15:14:19 +0530

    feature 3 :banner using string join

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,32 @@
+/**
+ * OOPSBannerApp UC3 - OOPS Banner Application (Use Case 3)
+ * Uses String.join() instead of + operator for better readability and efficiency
+ */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
 
-        System.out.println("  ***    " + "  ***    " + " *****   " + "  ****  ");
-        System.out.println(" ** **   " + " ** **   " + " **  **  " + " **     ");
-        System.out.println("**   **  " + "**   **  " + " **  **  " + " **     ");
-        System.out.println("**   **  " + "**   **  " + " *****   " + "  ****  ");
-        System.out.println("**   **  " + "**   **  " + " **      " + "     ** ");
-        System.out.println(" ** **   " + " ** **   " + " **      " + "     ** ");
-        System.out.println("  ***    " + "  ***    " + " **      " + " ****   ");
+        // Each line represents O O P S
+
+        System.out.println(String.join(" ",
+                "  ***   ", "  ***   ", " *****  ", "  ****  "));
+
+        System.out.println(String.join(" ",
+                " ** **  ", " ** **  ", " **  ** ", " **     "));
+
+        System.out.println(String.join(" ",
+                "**   ** ", "**   ** ", " **  ** ", " **     "));
+
+        System.out.println(String.join(" ",
+                "**   ** ", "**   ** ", " *****  ", "  ****  "));
+
+        System.out.println(String.join(" ",
+                "**   ** ", "**   ** ", " **     ", "     ** "));
+
+        System.out.println(String.join(" ",
+                " ** **  ", " ** **  ", " **     ", "     ** "));
+
+        System.out.println(String.join(" ",
+                "  ***   ", "  ***   ", " **     ", " ****   "));
     }
 }
