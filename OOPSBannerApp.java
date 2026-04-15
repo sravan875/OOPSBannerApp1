97495c71320b36c5ab82aa8cf2bc3ea622cbd946
sravan875 <sravankumarkona875@gmail.com>
2026-04-15 15:37:58 +0530

    banner using inline array

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,22 @@
 /**
- * UC4 - OOPS Banner using Array and Loop
+ * UC5 - OOPS Banner using Inline Array Initialization
  */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Store all 7 lines in an array
-        String[] banner = new String[7];
+        // Inline array initialization
+        String[] banner = {
+            String.join(" ", "  ***   ", "  ***   ", " *****  ", "  ****  "),
+            String.join(" ", " ** **  ", " ** **  ", " **  ** ", " **     "),
+            String.join(" ", "**   ** ", "**   ** ", " **  ** ", " **     "),
+            String.join(" ", "**   ** ", "**   ** ", " *****  ", "  ****  "),
+            String.join(" ", "**   ** ", "**   ** ", " **     ", "     ** "),
+            String.join(" ", " ** **  ", " ** **  ", " **     ", "     ** "),
+            String.join(" ", "  ***   ", "  ***   ", " **     ", " ****   ")
+        };
 
-        banner[0] = String.join(" ", "  ***   ", "  ***   ", " *****  ", "  ****  ");
-        banner[1] = String.join(" ", " ** **  ", " ** **  ", " **  ** ", " **     ");
-        banner[2] = String.join(" ", "**   ** ", "**   ** ", " **  ** ", " **     ");
-        banner[3] = String.join(" ", "**   ** ", "**   ** ", " *****  ", "  ****  ");
-        banner[4] = String.join(" ", "**   ** ", "**   ** ", " **     ", "     ** ");
-        banner[5] = String.join(" ", " ** **  ", " ** **  ", " **     ", "     ** ");
-        banner[6] = String.join(" ", "  ***   ", "  ***   ", " **     ", " ****   ");
-
-        // Print using loop
+        // Loop to print
         for (String line : banner) {
             System.out.println(line);
         }
